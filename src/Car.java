5e8e464977cbee2a47058cd62762724ccf5ce49b
Perale Thomas <thomas.perale@openmailbox.org>
2015-11-22 17:19:05 +0100

    Ajout des fonctions qui vont permettre de modifier des Parkings.

--- src/Car.java
+++ src/Car.java
@@ -1,10 +1,86 @@
-public class Car {
-    int[] x_position;
-    int[] y_position;
+import java.util.EnumSet;
 
-    public void forward () {
+public class Car {
+    public enum Direction {
+        HORIZONTAL,
+        VERTICAL
     }
 
-    public void backward () {
+    private int[] x_position;
+    private int[] y_position;
+    private Direction dir;
+
+    public int[] get_position () {
+        // TODO Trouver un moyen d'avoir les positions
+        // ---> iterator ?
+    }
+
+    /* @desc Renvoie la position de la voiture si on doit
+     *      l'avancer.
+     *
+     * @return {Car} : Nouvelle voiture dans sa nouvelle position.
+     */
+    public Car forward () {
+        Car result;
+        if ( this.dir == HORIZONTAL ) {
+            // Change la position de tout les éléments
+            int newXpos[this.x_position.length];
+            for ( int i = 0; i < this.x_position.length; ++i ) {
+                newXpos[i] = (this.x_position[i] + 1);
+            }
+            result = new Car( newXpos,
+                              this.y_position, 
+                              this.dir
+                             )
+        } else {
+            // Change la position de tout les éléments
+            int newYpos[this.y_position.length];
+            for ( int i = 0; i < this.y_position.length; ++i ) {
+                newYpos[i] = (this.y_position[i] + 1);
+            }
+            result = new Car( this.x_position,
+                              newYpos,
+                              this.dir
+                             )
+        }
+        return result;
+    }
+
+    /* @desc Renvoie la position de la voiture si on doit
+     *      l'avancer.
+     *
+     * @return {Car} : Nouvelle voiture dans sa nouvelle position.
+     */
+    public Car backward () {
+        Car result;
+        if ( this.dir == HORIZONTAL ) {
+            // Change la position de tout les éléments
+            int newXpos[this.x_position.length];
+            for ( int i = 0; i < this.x_position.length; ++i ) {
+                newXpos[i] = (this.x_position[i] - 1);
+            }
+
+            result = new Car( newXpos,
+                              this.y_position, 
+                              this.dir
+                             )
+        } else {
+            // Change la position de tout les éléments
+            int newYpos[this.y_position.length];
+            for ( int i = 0; i < this.y_position.length; ++i ) {
+                newYpos[i] = (this.y_position[i] - 1);
+            }
+            result = new Car( this.x_position,
+                              newYpos,
+                              this.dir
+                             )
+        }
+        return result;
+    }
+
+    Car ( int[] x_pos, int[] y_pos, Direction dir) {
+        this.x_position = x_pos;
+        this.y_position = y_pos;
+        this.dir = dir;
     }
 }
--- src/Parking.java
+++ src/Parking.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
+import java.util.Iterator;
 import Car;
 
 public class Parking {
@@ -9,6 +10,76 @@ public class Parking {
     private int y_size;
     private Car goal_car;
     private Car[] carList;
+    private boolean[][] parkingMatrix;
+
+    /* http://stackoverflow.com/questions/5849154/can-we-write-our-own-iterator-in-java
+     * @desc Iterateur sur les véhicules de la classe pour acceder
+     *      au différents véhicules en dehors de la classe. 
+     */
+    @Override
+    public Iterator<Car> iterator () {
+        Iterator<Car> it = new Iterator<Car>();
+
+        private int currentIndex = 0;
+
+        @Override
+        public boolean hasNext() {
+            return currentIndex < carList.length;
+        }
+
+        @Override
+        public Car next() {
+            ++currentIndex;
+            carList[currentIndex];
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+    }
+
+    /* @desc Renvoie un la manière dont le parking serait si
+     *      la voiture {toMoveCar} était bougée en avant.
+     *
+     * @param {toMoveCar} : Voiture que l'on va devoir bouger.
+     *
+     * @return {Parking} : La nouvelle forme du parking.
+     */
+    public Parking move_forward( Car toMoveCar ) {
+        // Vérification que la voiture sait avancer.
+        Car newCarPos = toMoveCar.forward();
+        int[2] newPosition = newCarPos.position();
+        if ( newPosition[0] > this.x_size
+                || newPosition[1] > this.y_size
+                || this.parkingMatrix[newPosition[0]][newPosition[1]] ) {
+            return null;
+        }
+
+        // COPY de {carList} mais en remplaçant {toMoveCar}.
+        Car[] result = new Car[this.carList.length];
+
+        for ( int i = 0; i < this.carList.length(); ++i ) {
+            if ( this.carList[i] == toMoveCar ) {
+                result[i] = toMoveCar.forward()
+            } else {
+                result[i] = carElem.copy();
+            }
+        }
+
+        return new Parking(this.x_size, this.y_size, result);
+    }
+
+    /* @desc Renvoie un la manière dont le parking serait si
+     *      la voiture {toMoveCar} était bougée en arrière.
+     *
+     * @param {toMoveCar} : Voiture que l'on va devoir bouger.
+     *
+     * @return {Parking} : La nouvelle forme du parking.
+     */
+    public Parking move_backward( Car toMoveCar ) {
+        // TODO la même qu'au dessus.
+    }
 
     public Car set_goal_car (int x_pos, int y_pos) {
         this.goal_car = this.append_car (x_pos, y_pos); 
@@ -21,10 +92,33 @@ public class Parking {
     public Car append_car (int x_pos, int y_pos) {
         Car newCar = new Car(x_pos, y_pos);
         this.carList.add( newCar );
+
+        // TODO Prendre en compte que l'on peut avoir plusieurs positions.
+        // TODO On peut faire cette opération avant pour attraper 
+        //      les éventuels erreurs qu'il peut y avoir si il y a
+        //      déjà une voiture à cette position.
+        this.parkingMatrix[x_pos][y_pos] = true;
+
         return newCar;
     }
 
-    Parking (x_size, y_size) {
+    Parking (int x_size, int y_size) {
+        this.x_size = x_size;
+        this.y_size = y_size;
 
+        this.parkingMatrix = new boolean[x_size][y_size];
+    }
+
+    Parking (int x_size, int y_size, Car carList) {
+        this.x_size = x_size;
+        this.y_size = y_size;
+        this.carList = carList;
+
+        this.parkingMatrix = new boolean[x_size][y_size];
+        for () {
+            for () {
+                // TODO Ajouter le tout à la matrice.
+            }
+        }
     }
 }
