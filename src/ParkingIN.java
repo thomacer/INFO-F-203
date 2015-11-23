a89cfe0b4fa5dc403103fffa64018823f2138e0e
georgesrusu <georgesrusu@yahoo.fr>
2015-11-23 22:27:58 +0100

    version de ParkignIN sans coordonne de l'exit

--- src/ParkingIN.java
+++ src/ParkingIN.java
@@ -1,124 +1,133 @@
-import java.io.IOException;
-import java.io.FileReader;
+import java.util.Scanner;
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
-
-
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 public class ParkingIN {
-    // ATTRIBUTES
-    private String fileName;
-    private int[2] exit;
-    private int[[]] parking;
 
-    // METHODS
-    private int get_next_int(String line, int i) {
-        while (i < line.length()
-                && line[i] < '0'
-                && line[i] > '9') 
+	private String _filename;
+	private int _xSize;
+	private int _ySize;
+	private int _nbreGoal;
+	private int _nbreVoiture;
+	private int[][] _positionVoiture;
+	private int[] _exit;
+	
+	ParkingIN(String file){			//constructor
+		_filename=file;
+	}
+	
+	public void parse_input_file(){
+		BufferedReader file_reader=null;
+		try{
+			file_reader=new BufferedReader(new FileReader(this._filename));
+			this.parse_parking(file_reader);
+			this.parse_element(file_reader);
+			this.parse_emplaceme(file_reader);
+		}
+		catch( FileNotFoundException exception){
+			System.out.print("File not found!Try again");
+		}
+	}
+	
+	public void parse_parking(BufferedReader file){
+		try{
+			String line;
+			line=file.readLine();
+			this._xSize=this.get_next_int(line, line.indexOf(":"));
+			this._ySize=this.get_next_int(line, line.indexOf("s"));
+			//this._parking=new int[x_size][y_size]; //creation parking!
+			
+		}catch(IOException ioe){
+			System.out.print("Erreur --"+ioe.toString());
+		}
+	}
+	public void parse_element(BufferedReader file){
+		try{
+			String line;
+			line=file.readLine();
+			for (int i=0;i<=this._ySize*2+1;++i){
+				line=file.readLine();
+			}
+			this._nbreGoal=this.get_next_int(line, line.indexOf(":"));
+			line=file.readLine();
+			this._nbreVoiture=this.get_next_int(line, line.indexOf(":"));
+			
+			
+		}catch(IOException ioe){
+			System.out.print("Erreur --"+ioe.toString());
+		}
+	}
+	public void parse_emplaceme(BufferedReader file){
+		try{
+			String line;
+			line=file.readLine(); 		
+			for (int i=0;i<this._nbreGoal+this._nbreVoiture;++i){
+				line=file.readLine();	//chaque ligne
+				System.out.print(line);
+				int x=this.get_next_int(line, line.indexOf("("));//premier x coord
+				int y=this.get_next_int(line, line.indexOf(","));//premier y coord
+				System.out.print(x);			//premier x coord
+				System.out.print(y);			//premier y coord
+				System.out.print(" ");
+				String temp="";		
+				for (int j=line.indexOf(", ")+1;j<line.length();++j){ 		//je decoupe la line a partir de ", "
+					temp+=line.charAt(j);									//pour les seconde parametre
+				}	
+				int xt=this.get_next_int(temp, temp.indexOf("(")); 	//deuximee x coord
+				int yt=this.get_next_int(temp, temp.indexOf(","));	//deuxiem y coord
+				System.out.print(" ");
+				System.out.print(xt);	//deuximee x coord
+				System.out.print(yt);	//deuxiem y coord
+				
+			}
+			
+		}catch(IOException ioe){
+			System.out.print("Erreur --"+ioe.toString());
+		}
+	}
+	private int get_next_int(String line, int i) {
+		boolean cycle=true;
+        while (i < line.length() && cycle)
         {
-            i++;
+        	if (Character.isDigit(line.charAt(i))){
+        		cycle=false;
+        	}
+        	else{
+        		++i;
+        	}
         }
-        // À ce point i représente le moment où 
-        // les entiers commencent.
         int j = i;
-        while (j < line.length 
-                && line[j] >= '0'
-                && line[j] <= '9')
+        ++j;
+        cycle=true;
+        while (j < line.length() && cycle)
         {
-            // Tant que l'index pointe sur un entier.
-            ++j;
+        	if (!Character.isDigit(line.charAt(j))){
+        		cycle=false;
+        	}
+        	else{
+        		++j;
+        	}
         }
-
-        int result = Integer.parseInt(line[i:j]);
-
+        String res="";
+        for(int k=i;k<j;++k){
+        	res+=line.charAt(k);
+        }
+        int result = Integer.parseInt(res);
         return result;
     }
-
-    /* @desc Parse l'aspect du parking.
-     *  Dans cette partie, va être interprété:
-     *      Parking: 5 fois 5
-     *               ^      ^
-     *               |      |
-     *               x      y
-     *
-     *      +---+---+---+---+---+
-     *      |                   |
-     *      + ...
-     *
-     * @param file Opened file.
-     *
-     * @return Une valeur qui nous laisse savoir 
-     *      si l'exécution s'est bien passé ou non.
-     */
-    private void parse_parking (BufferedReader file) {
-        String line = file.readLine(); 
-        int i = 0;
-        int x_size = get_next_int(line, i);
-        int y_size = get_next_int(line, i);
-       
-        // Avoir la taille dans une des premières ligne.
-        delimiters = "+---" * x_size;
-        // endOfLine = "+" + "---+" * size;
-        separator_1 = "+" + "   +" * size;
-        separator_2 = "|" + "    " * (size - 1) + "   |";
-        // Deuxième type de séparateur qui doit comporter une sortie.
-        while ( delimiters != file.readLine() )
-            // imiter le in de python.
-
-        int i = 0
-        line = file.readLine();
-        while ( i < y_size ) {
-            line.readLine();
-            if ( line != separator_2 ) {
-                // set there is the exit.                
-            }
-        }
-        // Le fichier se met à la fin de la représentation du parking
-
-        // line should be 
-    }
-
-    /* @desc Parse l'aspect du parking.
-     *  Ex:
-     *      Elements du Parking:
-     *          voiture Goal: <Nombre de voitures Goal>
-     *          Autres voitures: <Nmbre de voitures autres>
-     *
-     *  --------------------------
-     *
-     * @param file Opened file.
-     *
-     * @return Une valeur qui nous laisse savoir 
-     *      si l'exécution s'est bien passé ou non.
-     */
-    private void parse_elements (BufferedReader file) {
-    }
-
-    /* @desc Parse l'aspect du parking.
-     *
-     * @param file Opened file.
-     *
-     * @return Une valeur qui nous laisse savoir 
-     *      si l'exécution s'est bien passé ou non.
-     */
-    private void parse_position (BufferedReader file) {
-        
-    }
-
-    public void parse_input_file () {
-        /* 1) Open file
-         * 2) Parse parking
-         * 3) Parse elements
-         * 4) Parse position
-         */
-        BufferedReader file_reader = new BufferedReader(new FileReader(this.fileName));
-        parse_parking(file_reader);
-        parse_elements(file_reader);
-        parse_position(file_reader);
-    }
-
-    Parking(String fileName) {
-        this.fileName = fileName;
-    }
+	public static void main(String args[]){
+		Scanner scan=new Scanner(System.in);
+		System.out.print("Enter file name :");
+		String file=scan.nextLine();
+		ParkingIN test=new ParkingIN(file);
+		test.parse_input_file();
+		
+		
+		
+		
+	}
+	
 }
